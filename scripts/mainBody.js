56c5ac3da4fcd37a168d46bc71159ed072f344f2
Akshat <akshat.03104@gmail.com>
2025-08-13 16:57:35 +0530

    working version of carousel

--- scripts/mainBody.js
+++ scripts/mainBody.js
@@ -5,6 +5,7 @@ const carouselImagesLocation = ['/images/carousel-images/first.jpg',
     '/images/carousel-images/fifth.jpg'];
 
 const carouselContainer = document.getElementById("carousel");
+
 const imgElementsArray = Array.from(carouselImagesLocation.map(location => {
     const imgContainer = document.createElement("img");
     imgContainer.src = location;
@@ -12,21 +13,21 @@ const imgElementsArray = Array.from(carouselImagesLocation.map(location => {
     return imgContainer;
 }));
 
-function createCarouselNavFor(elementCount){
+function createCarouselNavFor(elementCount) {
     const container = document.createElement("div");
-    while(elementCount > 0){
+    while (elementCount > 0) {
         const button = document.createElement("button");
         button.classList.add("carouselNavButtons");
         container.appendChild(button);
         elementCount--;
     }
     container.id = "carouselNavBar";
-    container.firstElementChild.classList.add("activeNavButton");
+    container.firstElementChild.classList.add("activeCarouselNavButton");
     return container;
 }
 
 function appendCarouselElementsToContiner(carouselContainer, carouselElementsArray) {
-    carouselElementsArray[0].classList.add("activeCarouselImg");
+    carouselElementsArray[0].classList.add("activeCarouselImage");
     carouselElementsArray.forEach(element => {
         carouselContainer.appendChild(element);
     });
@@ -35,3 +36,49 @@ function appendCarouselElementsToContiner(carouselContainer, carouselElementsArr
 
 
 appendCarouselElementsToContiner(carouselContainer, imgElementsArray);
+
+
+// ******************************** ADDING FUNCTIONALITY TO CAROUSEL *************************************** //
+
+const carouselImages = document.querySelectorAll('.carouselImages');
+const carouselNavButtons = document.querySelectorAll('.carouselNavButtons');
+let currentIndex = 0;
+
+
+function resetOtherButtonsAndImagesInCarousel(){
+    carouselImages.forEach(image =>{
+        image.classList.remove("activeCarouselImage");
+    })
+    carouselNavButtons.forEach(button =>{
+        button.classList.remove('activeCarouselNavButton');
+    })
+};
+
+
+carouselNavButtons.forEach((button, index) => {
+    button.addEventListener('click', ()=>{
+
+        resetOtherButtonsAndImagesInCarousel();
+
+        carouselImages[index].classList.add("activeCarouselImage");
+        button.classList.add("activeCarouselNavButton");
+        currentIndex = index;
+    })
+})
+
+
+// below code automates the slideshow to happen continuously
+
+
+function goToIndex(index){
+    resetOtherButtonsAndImagesInCarousel();
+    carouselImages[index].classList.add("activeCarouselImage");
+    carouselNavButtons[index].classList.add("activeCarouselNavButton");
+}
+
+function slideshowCarousel(){
+    currentIndex = (currentIndex+1) % carouselImages.length;
+    goToIndex(currentIndex);
+}
+
+setInterval(slideshowCarousel, 4000);
